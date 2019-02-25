3b83cf821db4689b6c48915de1c0d75860ff2acb
doodybrains <doodybrainz@gmail.com>
2019-02-25 16:19:32 -0500

    add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# computarot
+
+meditation for Electronic Rituals, Oracles and Fortune Telling / Spring 2019 with Allison Parrish
+
+this is a bash script for a computer to give itself a tarot reading :)
---
+++ folder-tarot.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ -d "computarot-reading" ]; then
+  rm -r computarot-reading
+  
+  mkdir computarot-reading
+
+  cd computarot-reading
+
+  for run in {1..3}
+  do
+    foo=$(curl 'https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json')
+
+    x=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].name'`
+
+    y=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].keywords[0]'`
+
+    mkdir "${x//\"}"
+    cd "${x//\"}"
+    open .
+    mkdir "${y//\"}"
+    mdfind "${y//\"}" | head -3 | while IFS= read -r filez; do cp "$filez" "${y//\"}"; done
+    cd "${y//\"}"
+    open .
+    cd ..
+    cd ..
+  done
+elif [ ! -d "computarot-reading" ]; then
+  mkdir computarot-reading
+
+  cd computarot-reading
+
+  for run in {1..3}
+  do
+    foo=$(curl 'https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json')
+
+    x=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].name'`
+
+    y=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].keywords[0]'`
+
+    mkdir "${x//\"}"
+    cd "${x//\"}"
+    open .
+    mkdir "${y//\"}"
+    mdfind "${y//\"}" | head -3 | while IFS= read -r filez; do cp "$filez" "${y//\"}"; done
+    cd "${y//\"}"
+    open .
+    cd ..
+    cd ..
+  done
+fi
