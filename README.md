bcbb975fe8b5df059c7d2a82cfbe0d6708fb7880
doodybrains <doodybrainz@gmail.com>
2019-02-25 16:28:23 -0500

    Update

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 meditation for Electronic Rituals, Oracles and Fortune Telling / Spring 2019 with Allison Parrish
 
 this is a bash script for a computer to give itself a tarot reading :)
+
+#### to try it out follow these steps
+
+clone or download this repository
+
+open a new terminal window
+
+`$ cd computarot-code`
+
+`./folder-tarot.sh`
--- folder-tarot.sh
+++ folder-tarot.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-if [ -d "computarot-reading" ]; then
-  rm -r computarot-reading
-  
-  mkdir computarot-reading
-
-  cd computarot-reading
-
+do_reading() {
   for run in {1..3}
   do
     foo=$(curl 'https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json')
@@ -25,27 +18,23 @@ if [ -d "computarot-reading" ]; then
     cd ..
     cd ..
   done
+}
+
+cd ..
+
+if [ -d "computarot-reading" ]; then
+  rm -r computarot-reading
+
+  mkdir computarot-reading
+
+  cd computarot-reading
+
+  do_reading
+
 elif [ ! -d "computarot-reading" ]; then
   mkdir computarot-reading
 
   cd computarot-reading
 
-  for run in {1..3}
-  do
-    foo=$(curl 'https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json')
-
-    x=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].name'`
-
-    y=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].keywords[0]'`
-
-    mkdir "${x//\"}"
-    cd "${x//\"}"
-    open .
-    mkdir "${y//\"}"
-    mdfind "${y//\"}" | head -3 | while IFS= read -r filez; do cp "$filez" "${y//\"}"; done
-    cd "${y//\"}"
-    open .
-    cd ..
-    cd ..
-  done
+  do_reading
 fi
