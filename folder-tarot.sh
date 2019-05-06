23a5ef92d2f0ac2e247812a9fe59b3dfcd24b6b6
doodybrains <doodybrainz@gmail.com>
2019-05-06 14:23:53 -0400

    update third

--- folder-tarot.sh
+++ folder-tarot.sh
@@ -4,19 +4,20 @@ do_reading() {
   do
     foo=$(curl 'https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json')
 
-    x=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].name'`
-
-    y=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].keywords[0]'`
-
-    mkdir "${x//\"}"
-    cd "${x//\"}"
-    open .
-    mkdir "${y//\"}"
-    mdfind "${y//\"}" | head -3 | while IFS= read -r filez; do cp "$filez" "${y//\"}"; done
-    cd "${y//\"}"
-    open .
-    cd ..
-    cd ..
+    # x=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].name'`
+    echo ${foo}
+    echo ${x}
+    # y=`echo $foo | jq --arg random $((0 + RANDOM % 78)) '.["tarot_interpretations"] | .[$random|tonumber].keywords[0]'`
+    #
+    # mkdir "${x//\"}"
+    # cd "${x//\"}"
+    # open .
+    # mkdir "${y//\"}"
+    # mdfind "${y//\"}" | head -3 | while IFS= read -r filez; do cp "$filez" "${y//\"}"; done
+    # cd "${y//\"}"
+    # open .
+    # cd ..
+    # cd ..
   done
 }
 
